bb6ab740e49726b5b03565733b7d51f150347e9a
Sanaya James <sanaya.j.james@gmail.com>
2026-05-14 14:45:27 -0700

    set up for deployment

--- server/app.py
+++ server/app.py
@@ -6,6 +6,8 @@ load_dotenv()
 from flask import Flask, jsonify, request, make_response, render_template
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
+from dotenv import load_dotenv
+load_dotenv()
 
 from models import db, Bird
 
